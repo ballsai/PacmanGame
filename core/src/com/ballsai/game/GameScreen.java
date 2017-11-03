331202a718708859588fc81ab3a8aa85e1abcbd6
ballsai <jarunemail@gmail.com>
2017-11-03 16:05:56 +0700

    Pacman can move 4 directions

--- core/src/com/ballsai/game/GameScreen.java
+++ core/src/com/ballsai/game/GameScreen.java
@@ -1,6 +1,7 @@
 package com.ballsai.game;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
@@ -21,16 +22,26 @@ public class GameScreen extends ScreenAdapter{
     }
     
     public void render (float delta) {
+    	 update(delta);
     	Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-    	 x += 5;
     	 SpriteBatch batch = pacmanGame.batch;
          batch.begin();
-        
          batch.draw(pacmanImg, x, y);
          batch.end();
-        
-         
-    	
+    }
+    private void update(float delta) {
+    	if(Gdx.input.isKeyPressed(Keys.LEFT)) {
+            x -= 10;
+        }
+        if(Gdx.input.isKeyPressed(Keys.RIGHT)) {
+            x += 10;
+        }
+        if(Gdx.input.isKeyPressed(Keys.DOWN)) {
+            y -= 10;
+        }
+        if(Gdx.input.isKeyPressed(Keys.UP)) {
+            y += 10;
+        }
     }
 }
